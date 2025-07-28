00e14c56f5e477a27a8e24d3f2542f5bc0c61b55
Olga Palushina <lokipypzemli@mail.ru>
2025-07-28 22:09:26 +0300

    add default value for browser_version and browser_size

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,11 +12,11 @@ public class TestBase {
 
     @BeforeAll
     static void basicBrowserSettings() {
-        Configuration.browserSize = System.getProperty("browser_size");
+        Configuration.browserSize = System.getProperty("browser_size", "1280x720");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browser =  System.getProperty ("browser","chrome");
-        Configuration.browserVersion =  System.getProperty("browser_version");
+        Configuration.browserVersion =  System.getProperty("browser_version", "128.0");
         Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url","selenoid.autotests.cloud/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
