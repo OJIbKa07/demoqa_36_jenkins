62269b4093df30ac3dc910e37280f130f86ebb5e
Olga Palushina <lokipypzemli@mail.ru>
2025-07-29 10:39:06 +0300

    swap beforeAll -> BeforeEach

--- src/test/java/tests/PracticeFormWithPagesObjectsTest.java
+++ src/test/java/tests/PracticeFormWithPagesObjectsTest.java
@@ -42,6 +42,18 @@ public class PracticeFormWithPagesObjectsTest extends TestBase {
 
     @BeforeEach
     void addListener() {
+        Configuration.browserSize = System.getProperty("browser_size", "1920x1080");
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browser =  System.getProperty ("browser","chrome");
+        Configuration.browserVersion =  System.getProperty("browser_version", "127.0");
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url","selenoid.autotests.cloud/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
